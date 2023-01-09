0052fc6a1544b07f4a41b5fa135965f3ad60e6b4
Your Name <you@example.com>
2023-01-09 12:55:47 +0000

    formv2

--- src/Form/CommentFormType.php
+++ src/Form/CommentFormType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +17,20 @@ class CommentFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('author')
-            ->add('text')
-            ->add('email')
-            ->add('createdAt')
-            ->add('note')
-            ->add('conference')
+            ->add('author', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('email', EmailType::class,[
+                'label' => 'Email'
+                ])
+            ->add('note', IntegerType::class, [
+                'label' => 'Note',
+                'required' => false
+                ])
+            ->add('text', TextareaType::class,[
+                'label' => 'Commentaire'
+                ])
+            ->add('Enregistrer', SubmitType::class)
         ;
     }
 
